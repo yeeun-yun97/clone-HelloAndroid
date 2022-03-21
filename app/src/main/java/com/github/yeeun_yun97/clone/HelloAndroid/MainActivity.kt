cccf2e016dc907aaed29540f76a4a0e7531da919
yyn9704@gmail.com <yyn9704@gmail.com>
2022-03-22 00:02:55 +0900

    0321 kotlin btn onclick func to simple and origin

--- app/src/main/java/com/github/yeeun_yun97/clone/HelloAndroid/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/clone/HelloAndroid/MainActivity.kt
@@ -21,12 +21,14 @@ class MainActivity : AppCompatActivity() {
         var myEdittext = findViewById<EditText>(R.id.my_edittext)
         var myButton = findViewById<Button>(R.id.my_btn)
 
+        //인자가 하나인 추상 메서드의 경우, 파라미터 이름도 안 정하고 it라는 이름으로 사용하여 아주 극적으로 코드를 줄일 수 있다.
         myButton.setOnClickListener {
             var btnLabel = (it as Button).text
             Toast.makeText(applicationContext, "button Label : ${btnLabel}", Toast.LENGTH_LONG)
                 .show()
         }
 
+        //클래식한 방법.
         myButton.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 var btnLabel = (v as Button).text
