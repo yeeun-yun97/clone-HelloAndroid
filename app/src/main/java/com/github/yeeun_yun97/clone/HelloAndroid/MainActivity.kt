886c9dad5436a407567e69667c5955a69e003a30
yyn9704@gmail.com <yyn9704@gmail.com>
2022-03-21 23:57:29 +0900

    0321 kotlin btn onclick func

--- app/src/main/java/com/github/yeeun_yun97/clone/HelloAndroid/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/clone/HelloAndroid/MainActivity.kt
@@ -2,7 +2,11 @@ package com.github.yeeun_yun97.clone.HelloAndroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,14 @@ class MainActivity : AppCompatActivity() {
         val greetingMsg = resources.getText(R.string.greeting).toString()
         val redColor = ContextCompat.getColor(applicationContext, R.color.red)
 
+        var myEdittext = findViewById<EditText>(R.id.my_edittext)
+        var myButton = findViewById<Button>(R.id.my_btn)
 
+        myButton.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v:View?){
+                var btnLabel = (v as Button).text
+                Toast.makeText(applicationContext,"button Label : ${btnLabel}", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
