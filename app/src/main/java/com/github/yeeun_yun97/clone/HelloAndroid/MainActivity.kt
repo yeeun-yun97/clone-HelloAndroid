e3bbbd37ceeaf52a39c74b4b11d9af71b5e4df34
yyn9704@gmail.com <yyn9704@gmail.com>
2022-03-22 00:01:49 +0900

    0321 kotlin btn onclick func to simple and origin

--- app/src/main/java/com/github/yeeun_yun97/clone/HelloAndroid/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/clone/HelloAndroid/MainActivity.kt
@@ -14,19 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var myTextView : TextView = findViewById(R.id.my_textview)
+        var myTextView: TextView = findViewById(R.id.my_textview)
         val greetingMsg = resources.getText(R.string.greeting).toString()
         val redColor = ContextCompat.getColor(applicationContext, R.color.red)
 
         var myEdittext = findViewById<EditText>(R.id.my_edittext)
         var myButton = findViewById<Button>(R.id.my_btn)
 
-        myButton.setOnClickListener{
+        myButton.setOnClickListener {
             var btnLabel = (it as Button).text
-            Toast.makeText(applicationContext,"button Label : ${btnLabel}", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "button Label : ${btnLabel}", Toast.LENGTH_LONG)
+                .show()
         }
 
-
+        myButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                var btnLabel = (v as Button).text
+                Toast.makeText(applicationContext, "button Label : ${btnLabel}", Toast.LENGTH_LONG)
+                    .show()
+            }
+        })
 
 
     }
