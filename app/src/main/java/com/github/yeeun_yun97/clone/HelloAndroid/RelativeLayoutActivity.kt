af345bf47cd8649676265fc20e95c242dbfb03fd
yyn9704@gmail.com <yyn9704@gmail.com>
2022-03-25 20:59:42 +0900

    0325 relative layout

---
+++ app/src/main/java/com/github/yeeun_yun97/clone/HelloAndroid/RelativeLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.github.yeeun_yun97.clone.HelloAndroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
